61eb0dbe7e3c0e16a0f39018b7ac4dc739e2ba50
Arsames Qajar <arsamesqajar@Arsamess-MacBook-Pro.local>
2018-03-23 10:55:10 -0700

    index.js file added

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// require('./models/User');
+// require('./models/Survey');
+// require('./services/passport');
+
+mongoose.Promise = global.Promise;
+// mongoose.connect(keys.mongoUri);
+const app = express();
+
+app.use(bodyParser.json());
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [keys.cookieKey]
+//   })
+// );
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // express serve up prod files
+  // if it doesnt exist, look in react-ui/build directory for said file
+  app.use(express.static('client/build'));
+  // any route we dont understand after looking in the react-ui/build, serve up index.html document
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
